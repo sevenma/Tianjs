daa6f185d5ab0d8fa1e71e581a98e51fee0b3e0e
yidanlin <137763911@qq.com>
2017-02-16 15:23:33 +0800

    验证码生成

--- src/main/java/com/tianjs/Application.java
+++ src/main/java/com/tianjs/Application.java
@@ -25,6 +25,8 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.github.pagehelper.PageHelper;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
 
 @EnableAutoConfiguration
 @SpringBootApplication
@@ -99,6 +101,7 @@ public class Application  extends WebMvcConfigurerAdapter {
       registry.addViewController("/registration").setViewName("register");
       registry.addViewController("/login").setViewName("login");
       registry.addViewController("/loginOut").setViewName("home");
+      registry.addViewController("/kaptcha").setViewName(null);
     }
 
     @Bean
@@ -106,6 +109,30 @@ public class Application  extends WebMvcConfigurerAdapter {
       return container -> container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/error"), new ErrorPage(HttpStatus.FORBIDDEN, "/error"));
     }
     
+    @Bean
+    public DefaultKaptcha captchaProducer() {
+        Properties properties = new Properties();
+        properties.put("kaptcha.border", "yes");
+        properties.put("kaptcha.border.color", "lightGray");
+        properties.put("kaptcha.textproducer.font.color", "darkGray");
+        properties.put("kaptcha.image.width", "160");
+        properties.put("kaptcha.image.height", "50");
+        properties.put("kaptcha.textproducer.font.size", "40");
+        properties.put("kaptcha.session.key", "kaptcha");
+        properties.put("kaptcha.textproducer.char.length", "4");
+        properties.put("kaptcha.background.clear.to", "gray");
+//        properties.put("kaptcha.noise.color", "blue");//增加干扰线
+        properties.put("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");//去掉干扰线
+        properties.put("kaptcha.textproducer.char.space", "6");
+        properties.put("kaptcha.textproducer.font.names", "宋体,楷体,微软雅黑");
+
+        Config config = new Config(properties);
+        DefaultKaptcha kaptcha = new DefaultKaptcha();
+        kaptcha.setConfig(config);
+
+        return kaptcha;
+    }
+
     /**
      * Start
      */
